537aca9d4297213cb3b23fabd46fa470315da4b2
kimdoha <kkhhjj3393@gmail.com>
2022-08-23 04:13:56 +0900

    Feat: product module 생성(#6)
    -

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { UsersModule } from 'src/users/users.module';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Product]),
+    UsersModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
