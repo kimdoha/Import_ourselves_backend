fdd6528e3ffda90796307544e93a936fe772eedf
kimdoha <kkhhjj3393@gmail.com>
2022-08-24 14:52:06 +0900

    Fix: cors 주석 해제(#17)
    -

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,17 @@ import {
 } from './app.swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //{ cors: true }
+  const app = await NestFactory.create(AppModule, { cors: true } );
+  
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('/docs', app, document, swaggerCustomOptions);
 
-  // app.enableCors({ 
-  //   origin: true,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
+  app.enableCors({ 
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(3000);
   console.info('Server is running on port 3000');
